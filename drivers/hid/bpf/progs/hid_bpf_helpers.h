5150a9c8b5c2e2036c124b070cd9de46e4da8037
Jarrett Schultz <jaschultz@microsoft.com>
2026-06-09 04:40:53 +0000

    HID: Add BUS_SPI support and define HID_SPI_DEVICE macro
    If connecting a hid_device with bus field indicating BUS_SPI print out
    "SPI" in the debug print.
    
    Macro sets the bus field to BUS_SPI and uses arguments to set vendor
    product fields.
    
    [Leon: mirror BUS_SPI in the HID-BPF helper enum used by this tree.]
    
    Link: https://patchew.org/linux/20260609-send-upstream-v4-0-b843d5e6ced3@chromium.org/20260609-send-upstream-v4-2-b843d5e6ced3@chromium.org/
    Link: https://github.com/ooaklee/linux_ms_dev_kit-sp11/commit/61bb2379f70997948c46f07e55ec85f4977f6310
    Signed-off-by: Dmitry Antipov <dmanti@microsoft.com>
    Reviewed-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Signed-off-by: Jingyuan Liang <jingyliang@chromium.org>
    Signed-off-by: Leon Silcott <leon@boasi.io>

--- drivers/hid/bpf/progs/hid_bpf_helpers.h
+++ drivers/hid/bpf/progs/hid_bpf_helpers.h
@@ -148,6 +148,7 @@ DEFINE_GUARD(bpf_spin, struct bpf_spin_lock, bpf_spin_lock, bpf_spin_unlock);
 #define BUS_AMIGA		0x16
 #define BUS_ADB			0x17
 #define BUS_I2C			0x18
+#define BUS_SPI			0x1C
 #define BUS_HOST		0x19
 #define BUS_GSC			0x1A
 #define BUS_ATARI		0x1B
